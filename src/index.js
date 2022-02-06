099a8d1b1d399698aff517743d55916b394b22bd
novikov-al-st <novikov.al.st@gmail.com>
2022-02-06 21:33:00 +0200

    feat: add method implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBracketsDictionary = [];
+  let closeBracketsDictionary = [];
+  let closeBrackets = [];
+
+  bracketsConfig.forEach(arr => {
+    openBracketsDictionary.push(arr[0]);
+    closeBracketsDictionary.push(arr[1]);
+  });
+
+  for (let i = 0; i < str.length; i++) {
+        let lastCloseBracket = closeBrackets[closeBrackets.length -1];
+
+        if(str[i] === lastCloseBracket)
+        {
+          closeBrackets.pop();
+          continue;
+        }
+
+        if(openBracketsDictionary.includes(str[i]))
+        {
+          closeBrackets.push(closeBracketsDictionary[openBracketsDictionary.indexOf(str[i])]);
+        } 
+        else {
+          return false;
+        }
+  }
+  return closeBrackets.length === 0;
 }
